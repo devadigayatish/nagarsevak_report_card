9b1cc6c68676bb10d94e44a60868fa5376cace5a
Sharvari Gaikwad <sharvari.v.gaikwad@gmail.com>
2016-06-14 20:09:10 +0530

    Added Download Data bar dynamically.

--- downloaded-data-bar.php
+++ downloaded-data-bar.php
@@ -1,21 +1,17 @@
-<!-- 
-    File:           downloaded-data-bar.php
-    Date:           03-06-2016
-
-    This script loads the downloaded data bar.
--->
 <?php
+//This script loads the downloaded data bar
+
 $q = intval($_GET['a']);
 require_once('includes/db_connection.php');
 $prabhag_num_A = $q . "A";
 $prabhag_num_B = $q . "B";
 
 
-echo "<h4 style='text-align: center;margin-top: 100px;'>Download Data:</h4>";
-echo "<div id='downloadList' class='btn-group' style='text-align: center;'>";
-        echo "<a class='btn btn-default ' target='_self' rel='' href='' data-slimstat-clicked='false' data-slimstat-type='2' data-slimstat-tracking='false' data-slimstat-callback='false' data-slimstat-async='false'>Overall</a>";
-        echo "<a class='btn btn-default ' target='_self' rel='' href='csv/" . $prabhag_num_A . ".csv' data-slimstat-clicked='false' data-slimstat-type='2' data-slimstat-tracking='false' data-slimstat-callback='false' data-slimstat-async='false'>Prabhag" . $prabhag_num_A . "</a>";
-        echo "<a class='btn btn-default ' target='_self' rel='' href='csv/" . $prabhag_num_B . ".csv' data-slimstat-clicked='false' data-slimstat-type='2' data-slimstat-tracking='false' data-slimstat-callback='false' data-slimstat-async='false'>Prabhag" . $prabhag_num_B . "</a>";
-        echo "<a class='btn btn-default ' target='_self' rel='' href='' data-slimstat-clicked='false' data-slimstat-type='2' data-slimstat-tracking='false' data-slimstat-callback='false' data-slimstat-async='false'>Original RTI Replies</a>";
+echo "<h3>Download Data</h3>";
+echo "<div id='downloadList'>";
+        echo "<a class='btn btn-primary' target='_self' rel='' href='' data-slimstat-clicked='false' data-slimstat-type='2' data-slimstat-tracking='false' data-slimstat-callback='false' data-slimstat-async='false'>Overall</a>&nbsp";
+        echo "<a class='btn btn-primary' target='_self' rel='' href='csv/" . $prabhag_num_A . ".csv' data-slimstat-clicked='false' data-slimstat-type='2' data-slimstat-tracking='false' data-slimstat-callback='false' data-slimstat-async='false'>Prabhag " . $prabhag_num_A . "</a>&nbsp";
+        echo "<a class='btn btn-primary' target='_self' rel='' href='csv/" . $prabhag_num_B . ".csv' data-slimstat-clicked='false' data-slimstat-type='2' data-slimstat-tracking='false' data-slimstat-callback='false' data-slimstat-async='false'>Prabhag " . $prabhag_num_B . "</a>&nbsp";
+        echo "<a class='btn btn-primary' target='_self' rel='' href='' data-slimstat-clicked='false' data-slimstat-type='2' data-slimstat-tracking='false' data-slimstat-callback='false' data-slimstat-async='false'>Original RTI Replies</a>&nbsp";
 echo "</div>";
 ?>
