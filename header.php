2274ed403b0502e80eee0d5160db62e5b40f9985
Yatish Devadiga <devadigayatish@gmail.com>
2016-06-17 01:47:15 +0530

    Moved the inclusion of header.php inside head section of html on all pages. Added the javascripts into header.php

--- header.php
+++ header.php
@@ -1,50 +1,51 @@
-    <!-- Animate.css -->
-    <link rel="stylesheet" href="<?php echo SITE_URL; ?>assets/css/animate.css">
-    <!-- Icomoon Icon Fonts-->
-    <link rel="stylesheet" href="<?php echo SITE_URL; ?>assets/css/icomoon.css">
-    <!-- Bootstrap  -->
-    <link rel="stylesheet" href="<?php echo SITE_URL; ?>assets/css/bootstrap.css">
-    <!-- Superfish -->
-    <link rel="stylesheet" href="<?php echo SITE_URL; ?>assets/css/superfish.css">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>Nagarsevak Report Card</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="Free HTML5 Template by FREEHTML5.CO" />
+<meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive" />
+<meta name="author" content="FREEHTML5.CO" />
 
-    <link rel="stylesheet" href="<?php echo SITE_URL; ?>assets/css/style.css">
+<!-- Facebook and Twitter integration -->
+<meta property="og:title" content=""/>
+<meta property="og:image" content=""/>
+<meta property="og:url" content=""/>
+<meta property="og:site_name" content=""/>
+<meta property="og:description" content=""/>
+<meta name="twitter:title" content="" />
+<meta name="twitter:image" content="" />
+<meta name="twitter:url" content="" />
+<meta name="twitter:card" content="" />
 
+<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+<link rel="shortcut icon" href="favicon.ico">
 
-        <link rel="stylesheet" href="<?php echo SITE_URL; ?>assets/css/leaflet.css" />
-        <link rel="stylesheet"href="<?php echo SITE_URL; ?>assets/css/leaflet.label.css" />
+<link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,300' rel='stylesheet' type='text/css'>
 
-        <script src="<?php echo SITE_URL ?>assets/js/lib/leaflet.js"></script>
-        <script src="<?php echo SITE_URL ?>assets/js/lib/leaflet-omnivore.min.js"></script>
-        <script src="<?php echo SITE_URL ?>assets/js/lib/leaflet.label.js"></script>
-        <script src="<?php echo SITE_URL ?>assets/js/lib/highcharts.js"></script>
+<!-- Animate.css -->
+<link rel="stylesheet" href="<?php echo SITE_URL; ?>assets/css/animate.css">
+<!-- Icomoon Icon Fonts-->
+<link rel="stylesheet" href="<?php echo SITE_URL; ?>assets/css/icomoon.css">
+<!-- Bootstrap  -->
+<link rel="stylesheet" href="<?php echo SITE_URL; ?>assets/css/bootstrap.css">
+<!-- Superfish -->
+<link rel="stylesheet" href="<?php echo SITE_URL; ?>assets/css/superfish.css">
 
+<link rel="stylesheet" href="<?php echo SITE_URL; ?>assets/css/style.css">
 
-    <!-- Modernizr JS -->
-    <script src="<?php echo SITE_URL ?>assets/js/lib/modernizr-2.6.2.min.js"></script>
-    <!-- FOR IE9 below -->
-    <!--[if lt IE 9]>
-    <script src="<?php echo SITE_URL ?>assets/js/lib/respond.min.js"></script>
-    <![endif]-->
+<link rel="stylesheet" href="<?php echo SITE_URL; ?>assets/css/leaflet.css" />
+<link rel="stylesheet"href="<?php echo SITE_URL; ?>assets/css/leaflet.label.css" />
 
-<div id="fh5co-header">
-    <header id="fh5co-header-section">
-        <div class="container">
-            <div class="nav-header">
-                <a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>
-                <h1 id="fh5co-logo"><a href="index.php">Nagarsevak <span>Report Card</span></a></h1>
-                <!-- START #fh5co-menu-wrap -->
-                <nav id="fh5co-menu-wrap" role="navigation">
-                    <ul class="sf-menu" id="fh5co-primary-menu">
-                        <li class="active">
-                            <a href="index.php">Home</a>
-                        </li>
-                        <li><a target="_blank" href="http://parivartan-pune.blogspot.in/p/about-us.html">About Parivartan</a></li>
-                        <li><a href = "about-nagarsevak-report-card.php">About Nagarsevak Report Card</a></li>
-                        <li><a href = "summary.php">Summary</a></li>
-                        <li><a href="contact.php">Contact</a></li>
-                    </ul>
-                </nav>
-            </div>
-        </div>
-    </header>
-</div>
+<script src="<?php echo SITE_URL ?>assets/js/lib/leaflet.js"></script>
+<script src="<?php echo SITE_URL ?>assets/js/lib/leaflet-omnivore.min.js"></script>
+<script src="<?php echo SITE_URL ?>assets/js/lib/leaflet.label.js"></script>
+<script src="<?php echo SITE_URL ?>assets/js/lib/highcharts.js"></script>
+
+<!-- Modernizr JS -->
+<script src="<?php echo SITE_URL ?>assets/js/lib/modernizr-2.6.2.min.js"></script>
+<!-- FOR IE9 below -->
+<!--[if lt IE 9]>
+<script src="<?php echo SITE_URL ?>assets/js/lib/respond.min.js"></script>
+<![endif]-->
+
+<script src="<?php echo SITE_URL ?>assets/js/lib/jsapi.js"></script>
