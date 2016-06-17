570dc78b8b910ca96a9133a5cc2707f497f718f1
Sharvari Gaikwad <sharvari.v.gaikwad@gmail.com>
2016-06-17 18:48:43 +0530

    Fixed the issue of space after Download Data bar.

--- downloaded-data-bar.php
+++ downloaded-data-bar.php
@@ -7,7 +7,7 @@ $prabhag_num_A = $q . "A";
 $prabhag_num_B = $q . "B";
 
 
-echo "<h3>Download Data</h3>";
+echo "<div class='text-center'><h2>Download Data</h2></div>";
 echo "<div id='downloadList'>";
         echo "<a class='btn btn-primary' target='_self' rel='' href='' data-slimstat-clicked='false' data-slimstat-type='2' data-slimstat-tracking='false' data-slimstat-callback='false' data-slimstat-async='false'>Overall</a>&nbsp";
         echo "<a class='btn btn-primary' target='_self' rel='' href='csv/" . $prabhag_num_A . ".csv' data-slimstat-clicked='false' data-slimstat-type='2' data-slimstat-tracking='false' data-slimstat-callback='false' data-slimstat-async='false'>Prabhag " . $prabhag_num_A . "</a>&nbsp";
