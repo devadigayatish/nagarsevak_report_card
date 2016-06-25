7c0eb3ddc9099045c1168e2c4baf405251045c04
Yatish Devadiga <devadigayatish@gmail.com>
2016-06-25 23:31:57 +0530

    Moved dropdown code out of index.php

--- index.php
+++ index.php
@@ -164,115 +164,9 @@ require_once('includes/functions.php');
                     </div>
                     
                     <div class="col-md-6 col-sm-6 text-center" style="margin-top: 15px;">
-                     <a class="btn btn-primary" value="previous" onClick="nextPrabhag(-1)">Previous</a>&nbsp;
-                        <select class="input-group-sm" id="users" name="users" style="width:60%;" onchange="showUser(this.value)">
-                        <optgroup label='Aundh'>
-                            <option value='6'>Prabhag 6: Bopodi Gaonthan</option>
-                            <option value='7'>Prabhag 7: Pune University</option>
-                            <option value='8'>Prabhag 8: Aundh ITI</option>
-                            <option value='9'>Prabhag 9: Baner Balewadi</option>
-                            <option value='10'>Prabhag 10: Pashan Sutarwadi</option>
-                        </optgroup>
-                        <optgroup label='Bhawani Peth'>
-                            <option value='39'>Prabhag 39: KEM Hospital</option>
-                            <option value='47'>Prabhag 47: Arunkumar Vaidya Stadium</option>
-                            <option value='48'>Prabhag 48: Palkhi Vithoba Mandir</option>
-                            <option value='59'>Prabhag 59: Kashewadi Gurunanaknagar</option>
-                            <option value='60'>Prabhag 60: Mahatma Phule Peth Kashewadi Bhawani Peth</option>
-                            <option value='65'>Prabhag 65: Ghorpade Garden</option>
-                        </optgroup>
-                        <optgroup label='Bibvewadi'>
-                            <option value='64'>Prabhag 64: Salisbury Park</option>
-                            <option value='70'>Prabhag 70: Bibvewadi and KK Market</option>
-                            <option value='71'>Prabhag 71: Shivaji Market, Gultekdi</option>
-                            <option value='72'>Prabhag 72: Upper Indiranagar</option>
-                        </optgroup>
-                        <optgroup label='Dhankawadi'>
-                            <option value='69'>Prabhag 69: Dhankawadi Padmavati</option>
-                            <option value='73'>Prabhag 73: Balajinagar Vaibhavnagar</option>
-                            <option value='74'>Prabhag 74: Bharati Vidyapeeth</option>
-                            <option value='75'>Prabhag 75: Katraj Dairy</option>
-                            <option value='76'>Prabhag 76: Katraj Maulinagar</option>
-                        </optgroup>
-                        <optgroup label='Dhole Patil Road'>
-                            <option value='20' selected="selected">Prabhag 20: Magarpatta City</option>
-                            <option value='21'>Prabhag 21: Koregaon Park</option>
-                            <option value='22'>Prabhag 22: Naidu Hospital</option>
-                            <option value='23'>Prabhag 23: Junabazaar Kumbharwada</option>
-                            <option value='40'>Prabhag 40: Collectorate</option>
-                        </optgroup>
-                        <optgroup label='Ghole Road'>
-                            <option value='11'>Prabhag 11: Janwadi-Gokhalenagar</option>
-                            <option value='12'>Prabhag 12: Model Colony</option>
-                            <option value='13'>Prabhag 13: Narveer Tanajiwadi</option>
-                            <option value='24'>Prabhag 24: Balgandharva</option>
-                            <option value='25'>Prabhag 25: Wadarwadi-Pandavnagar</option>
-                            <option value='36'>Prabhag 36: Kamla Nehru Park</option>
-                        </optgroup>
-                        <optgroup label='Hadapsar'>
-                            <option value='42'>Prabhag 42: Lohiya Garden, Vaiduwadi</option>
-                            <option value='43'>Prabhag 43: Sadhana Vidyalay</option>
-                            <option value='44'>Prabhag 44: Gliding Centre</option>
-                            <option value='45'>Prabhag 45: Mohammadwadi</option>
-                        </optgroup>
-                        <optgroup label='Kasba Vishrambagwada'><option value='37'>Prabhag 37: Shaniwarwada</option>
-                            <option value='38'>Prabhag 38: Kasba Peth</option>
-                            <option value='49'>Prabhag 49: Dr. Kotnis Hospital</option>
-                            <option value='50'>Prabhag 50: Mahatma Phule Mandai</option>
-                            <option value='51'>Prabhag 51: Tilak Smarak Mandir</option>
-                            <option value='58'>Prabhag 58: Mahatma Phule Smarak</option>
-                        </optgroup>
-                        <optgroup label='Kondhwa Wanavdi'><option value='41'>Prabhag 41: Sopan Baug - Vikasnagar</option>
-                            <option value='46'>Prabhag 46: Wanavdi - Ramtekdi</option>
-                            <option value='61'>Prabhag 61: Kedar Vasti - Azadnagar</option>
-                            <option value='62'>Prabhag 62: Kondhwa Budruk, Gokulnagar</option>
-                            <option value='63'>Prabhag 63: Bhagyodaynagar</option>
-                        </optgroup>
-                        <optgroup label='Kothrud'>
-                            <option value='26'>Prabhag 26: MIT - Kelewadi</option>
-                            <option value='27'>Prabhag 27: Sutardara-Kishkindha Naga</option>
-                            <option value='28'>Prabhag 28: ARDE - Mhatobanagar</option>
-                            <option value='29'>Prabhag 29: Bavdhan Khurd</option>
-                            <option value='34'>Prabhag 34: Gujrat Colony</option>
-                        </optgroup>
-                        <optgroup label='Nagar Road'>
-                            <option value='2'>Prabhag 2: Kharadi Infotech Park</option>
-                            <option value='3'>Prabhag 3: Vimannagar Sanjay Park</option>
-                            <option value='4'>Prabhag 4: Nagpur Chawl</option>
-                            <option value='17'>Prabhag 17: Agakhan Palace</option>
-                            <option value='18'>Prabhag 18: Vadgaon Sheri</option>
-                            <option value='19'>Prabhag 19: Vadgaon Sheri - Ganesh Nagar</option>
-                        </optgroup>
-                        <optgroup label='Sahakarnagar'>
-                            <option value='57'>Prabhag 57: Kadakmal Ali Hirabaug</option>
-                            <option value='66'>Prabhag 66: Adinath Society</option>
-                            <option value='67'>Prabhag 67: Aranyeshwar Taware Colony</option>
-                            <option value='68'>Prabhag 68: Sahakarnagar - Taljai</option>
-                        </optgroup>
-                        <optgroup label='Tilak Road'>
-                            <option value='52'>Prabhag 52: Vaikunth Smashanbhoomi</option>
-                            <option value='53'>Prabhag 53: PL Deshpande Udyan</option>
-                            <option value='54'>Prabhag 54: Suncity Wadgaon Budruk</option>
-                            <option value='55'>Prabhag 55: Janata Vasahat</option>
-                            <option value='56'>Prabhag 56: Sarasbaug Parvati</option>
-                        </optgroup>
-                        <optgroup label='Warje Karvenagar'>
-                            <option value='35'>Prabhag 35: Deenanath Mangeshkar Hospital</option>
-                            <option value='30'>Prabhag 30: Popularnagar Malwadi</option>
-                            <option value='31'>Prabhag 31: Shivane - Warje - Ramnagar</option>
-                            <option value='32'>Prabhag 32: Karvenagar - Gosavivasti</option>
-                            <option value='33'>Prabhag 33: Dahanukar Colony</option>
-
-                        </optgroup>
-                        <optgroup label='Yerawda - Sangamwadi'>
-                            <option value='1'>Prabhag 1: Lohegaon Airport</option>
-                            <option value='5'>Prabhag 5: Vishrantwadi Mhaske Vasti</option>
-                            <option value='14'>Prabhag 14: Deccan College</option>
-                            <option value='15'>Prabhag 15: Rajguru Road</option>
-                            <option value='16'>Prabhag 16: Subhashnagar</option>
-                        </optgroup>
-                    </select>
-                    <a class="btn btn-primary"  value="next" onClick="nextPrabhag(1)">Next</a>
+                        <?php
+                            require_once('prabhag-dropdown.php');
+                        ?>
                         <div id="prabhag_no_info" class="row"></div>
                         <div id="txtHint" class="row"></div>
                     </div>
@@ -437,10 +331,3 @@ function nextPrabhag(i)
 nextPrabhag(0);
  </script>
 </html>
-
-
-
-
-
-
-
