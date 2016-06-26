8d9bf79d92b0534eda214974c0a95e1f9d271678
Yatish Devadiga <devadigayatish@gmail.com>
2016-06-26 12:33:01 +0530

    Updated the contents of Disclaimer page. Disclaimer page is now consistent with government websites

--- disclaimer.php
+++ disclaimer.php
@@ -46,20 +46,39 @@ require_once('includes/functions.php');
             </div>
 
             <div id="fh5co-about" style="padding-top: 50px;">
-
                 <div class="container">
                     <div class="row row-bottom-padded-md">
                         <div class="col-md-12 animate-box fadeInUp animated">
+                            <h3><strong> DISCLAIMER </strong></h3>
                             <p>
-                            <h2><strong> DISCLAIMER </strong></h2>
+                                Although information and contents on this portal have been put with care and diligence, Startup Partner Pvt Ltd and Parivartan does not take responsibility on how this information is used or the consequences of its use. In case of any inconsistency/confusion, the user should contact the concerned Department/Officer of Pune Municipal Corporation for further clarifications.
+                            </p>
+                            <hr>
+                            <h3><strong> TERMS And CONDITIONS </strong></h3>
+                            <p>
+                                This Website is designed, developed, hosted and maintained by Startup Partner Pvt Ltd. The content of this website is provided by Parivartan NGO and it is for information purposes only, enabling the public at large to have a quick and an easy access to information. Parivartan has obtained this data from various departments of Pune Municipal Corporation through Right to Information Act (RTI) 2005.
                                 <br>
-                                This Website is designed, developed, hosted and maintained by Startup Partner Pvt Ltd. The content of this website is provided by Parivartan and it is for information purposes only, enabling the public at large to have a quick and an easy access to information and do not have any legal sanctity.</p>
-
-                            </div>
+                                Efforts have been made to ensure the accuracy and correctness of the content on this website. However, the same should not be interpreted as a statement of law or used for any legal purposes. In case of any ambiguity or doubts, users are advised to verify / check with the concerned Department(s) and / or other source(s), and obtain appropriate professional advice.
+                                <br>
+                                Under no circumstances Startup Partner Pvt Ltd will be liable for any expense, loss or damage including, without limitation, indirect or consequential loss or damage or any expense, loss or damage whatsoever arising from use, or loss of use, of data, arising out of or in connection with the use of this website.
+                                <br>
+                                These terms and conditions shall be governed by and interpreted in accordance with the Indian Laws. Any dispute arising under these terms and conditions shall be subject to the jurisdiction of the courts of India.
+                            </p>
+                            <hr>
+                            <h3><strong> PRIVACY POLICY </strong></h3>
+                            <p>
+                                As a general rule, this portal does not automatically capture any specific personal information from you, (like name, phone number or e-mail address), that allows us to identify you individually. This portal records your visit and logs the following information for statistical purposes, such as Internet Protocol (IP) addresses, domain name, browser type, operating system, the date and time of the visit and the pages visited. We make no attempt to link these addresses with the identity of individuals visiting our site unless an attempt to damage the site has been detected. We will not identify users or their browsing activities, except when a law enforcement agency may exercise a warrant to inspect the service provider's logs. If this portal requests you to provide personal information, you will be informed how it will be used if you choose to give it and adequate security measures will be taken to protect your personal information.
+                            </p>
+                            <hr>
+                            <h3><strong> COPYRIGHT POLICY </strong></h3>
+                            <p>
+                                Material featured on this portal may be reproduced free of charge in any format or media without requiring specific permission. This is subject to the material being reproduced accurately and not being used in a derogatory manner or in a misleading context. Where the material is being published or issued to others, the source must be prominently acknowledged. However, the permission to reproduce this material does not extend to any material on this site which is identified as being the copyright of the third party. Authorisation to reproduce such material is obtained from the copyright holders concerned.
+                            </p>
+                            <hr>
                         </div>
-
                     </div>
                 </div>
+            </div>
 
             <?php
                 include'footer.php';
