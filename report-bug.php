d2ea30c6e432d45ff17ba9403aa5be4105803193
Yatish Devadiga <devadigayatish@gmail.com>
2016-06-27 08:40:30 +0530

    Moved the entire email sending code to a separate file.

--- report-bug.php
+++ report-bug.php
@@ -63,64 +63,9 @@ require_once('includes/email-settings.php');
                             </p>
 
                             <?php
-                            if(isset($_POST['submit'])){
-                                /*** This example shows settings to use when sending via Google's Gmail servers.*/
-                                //SMTP needs accurate times, and the PHP time zone MUST be set
-                                //This should be done in your php.ini, but this is how to do it if you don't have access to that
-                                date_default_timezone_set('Etc/UTC');
-                                require_once('includes/lib/PHPMailer-master/PHPMailerAutoload.php');
-                                //Create a new PHPMailer instance
-                                $mail = new PHPMailer;
-                                //Tell PHPMailer to use SMTP
-                                $mail->isSMTP();
-                                //Enable SMTP debugging
-                                // 0 = off (for production use)
-                                // 1 = client messages
-                                // 2 = client and server messages
-                                $mail->SMTPDebug = 0;
-                                //Ask for HTML-friendly debug output
-                                $mail->Debugoutput = 'html';
-                                //Set the hostname of the mail server
-                                $mail->Host = host;
-                                // use
-                                // $mail->Host = gethostbyname('smtp.gmail.com');
-                                // if your network does not support SMTP over IPv6
-                                //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
-                                $mail->Port = 465;
-                                //Set the encryption system to use - ssl (deprecated) or tls
-                                $mail->SMTPSecure = 'ssl';
-                                //Whether to use SMTP authentication
-                                $mail->SMTPAuth = auth;
-                                //Username to use for SMTP authentication - use full email address for gmail
-                                $mail->Username = Sender;
-                                //Password to use for SMTP authentication
-                                $mail->Password = Password;
-                                //Set who the message is to be sent from
-                                $mail->setFrom(Sender);
-                                //Set an alternative reply-to address
-                                $mail->addReplyTo(Sender);
-                                //Set who the message is to be sent to
-                                $mail->addAddress(Recipient);
-                                //Set the subject line
-                                $mail->Subject = 'Issue on Nagarsevak Report Card website';
-                                //Read an HTML message body from an external file, convert referenced images to embedded,
-                                //convert HTML into a basic plain-text alternative body
-                                $msg = "Issue reported by : " .$_POST['email']. " <br><br>Description of the issue : <br><br>" .$_POST['message']. " ";
-                                $mail->msgHTML($msg);
-                                //Replace the plain text body with one created manually
-                                $mail->AltBody = 'This is a plain-text message body';
-                                //send the message, check for errors
-                                if ($mail->send())
-                                {
-                                    echo "<div ><span style='color:#0000ff;'><strong>Thanks for reporting the issue. We will fix it ASAP !!</strong></span></div>";
-                                }
-                                else
-                                {
-                                    echo "<div><span style='color:#ff0000;'>Email sending failed !!<br>Please report the issue to devadigayatish@gmail.com</span></div>";
-                                }
-                            }
+                            require_once('includes/email-sender.php')
                             ?>
-<!--                             <div class="col-md-6">
+                            <!--<div class="col-md-6">
                                 <div class="form-group">
                                     <input type="text" id="txtEmail" required="" pattern="/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/"name="email" class="form-control" placeholder="Your Email">
                                 </div>
