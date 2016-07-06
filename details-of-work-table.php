66b9e9e06f0266457f7d60d27843feb1d2043867
Sharvari Gaikwad <sharvari.v.gaikwad@gmail.com>
2016-07-06 14:34:32 +0530

    Made details of work table dynamic for Years.

--- details-of-work-table.php
+++ details-of-work-table.php
@@ -1,5 +1,5 @@
 <?php
-$q = intval($_GET['m']);
+$q = intval($_GET['m']);;
 //MySQL Database Connect
 require_once('includes/db_connection.php');
 echo "<div class='text-center'><h2><strong>UTILIZATION OF WARD LEVEL FUNDS</strong></h2><br></div>";
@@ -7,96 +7,106 @@ echo "<div class='text-center'><h2><strong>UTILIZATION OF WARD LEVEL FUNDS</stro
 echo "<div class='col-md-6 col-sm-6'>";
     $prabhag_num_A = $q."A";
     echo "<div class='text-center'><h3>Prabhag No ".$prabhag_num_A." </h3></div>";
-    $sql_for_2012_2013="SELECT Year , Details_Of_Work , Amount FROM work_details WHERE Prabhag_No = '".$prabhag_num_A."' && Year = '2012 - 2013'";
-    $sql_for_2013_2014="SELECT Year , Details_Of_Work , Amount FROM work_details WHERE Prabhag_No = '".$prabhag_num_A."' && Year = '2013 - 2014'";
-    $sql_for_2014_2015="SELECT Year , Details_Of_Work , Amount FROM work_details WHERE Prabhag_No = '".$prabhag_num_A."' && Year = '2014 - 2015'";
-    $sql_for_2015_2016="SELECT Year , Details_Of_Work , Amount FROM work_details WHERE Prabhag_No = '".$prabhag_num_A."' && Year = '2015 - 2016'";
 
-    $result_for_2012_2013 = mysqli_query($con,$sql_for_2012_2013);
-    $result_for_2013_2014 = mysqli_query($con,$sql_for_2013_2014);
-    $result_for_2014_2015 = mysqli_query($con,$sql_for_2014_2015);
-    $result_for_2015_2016 = mysqli_query($con,$sql_for_2015_2016);
-
+    $sql = "SELECT DISTINCT Year FROM `work_details`";
+    $result = mysqli_query($con,$sql);
+    $num_results = $result->num_rows;
+    $YEAR =array();
+    for ($i=0; $i<$num_results; $i++) { 
+        $row = mysqli_fetch_array($result);
+        $YEAR[$i] = $row['Year'];
+    }
     $sql_for_total_amt="SELECT  SUM(Amount) AS Amount FROM work_details WHERE Prabhag_No = '".$prabhag_num_A."'";
     $result_for_total_amt = mysqli_query($con,$sql_for_total_amt);
     $row_for_total_amt = mysqli_fetch_array($result_for_total_amt);
 
         echo "<table class='table-bordered table-condensed'>";
-            echo "<colgroup><col style='width:17%;'><col style='width:63%;'><col style='width:20%;'></colgroup>";
+            echo "<colgroup><col style='width:19%;'><col style='width:61%;'><col style='width:20%;'></colgroup>";
             echo "<thead><tr><th><strong>Year</strong></th><th><strong>Details Of Work</strong></th><th><strong>Amount (Rs.)</strong></th></tr></thead>";
-                echo "<tbody class='table-odd'>";
-                while($row_for_2012_2013 = mysqli_fetch_array($result_for_2012_2013))
-                {
-                    echo "<tr><td>" . $row_for_2012_2013['Year'] . "</td><td>" . $row_for_2012_2013['Details_Of_Work'] . "</td><td>" . $row_for_2012_2013['Amount'] . "</td></tr>";
+            for ($i=0; $i < $num_results; $i++)
+            { 
+                $sql_for_select = "SELECT Year , Details_Of_Work , Amount FROM work_details WHERE Prabhag_No = '".$prabhag_num_A."' && Year = '".$YEAR[$i]."' ";
+                $result_for_select = mysqli_query($con,$sql_for_select);
+                $num_of_rows = $result_for_select->num_rows;
+                if ($i%2==0)
+                {   
+                    if($num_of_rows!=0)
+                    {
+                    echo "<tbody class='table-even'>";
+                    while($row_for_select = mysqli_fetch_array($result_for_select))
+                    {
+                        echo "<tr><td>" . $row_for_select['Year'] . "</td><td>" . $row_for_select['Details_Of_Work'] . "</td><td>" . $row_for_select['Amount'] . "</td></tr>";
+                    }
+                    echo "</tbody>";
+                    }   
                 }
-                echo "</tbody>";
-                echo "<tbody class='table-even'>";
-                while($row_for_2013_2014 = mysqli_fetch_array($result_for_2013_2014))
+                else
                 {
-                    echo "<tr><td>" . $row_for_2013_2014['Year'] . "</td><td>" . $row_for_2013_2014['Details_Of_Work'] . "</td><td>" . $row_for_2013_2014['Amount'] . "</td></tr>";
+                    if($num_of_rows!=0)
+                    {
+                    echo "<tbody class='table-odd'>";
+                    while($row_for_select = mysqli_fetch_array($result_for_select))
+                    {
+                        echo "<tr><td>" . $row_for_select['Year'] . "</td><td>" . $row_for_select['Details_Of_Work'] . "</td><td>" . $row_for_select['Amount'] . "</td></tr>";
+                    }
+                    echo "</tbody>";
+                    }
                 }
-                echo "</tbody>";
-                echo "<tbody class='table-odd'>";
-                while($row_for_2014_2015 = mysqli_fetch_array($result_for_2014_2015))
-                {
-                    echo "<tr><td>" . $row_for_2014_2015['Year'] . "</td><td>" . $row_for_2014_2015['Details_Of_Work'] . "</td><td>" . $row_for_2014_2015['Amount'] . "</td></tr>";
-                }
-                echo "</tbody>";
-                echo "<tbody class='table-even'>";
-                while($row_for_2015_2016 = mysqli_fetch_array($result_for_2015_2016))
-                {
-                    echo "<tr><td>" . $row_for_2015_2016['Year'] . "</td><td>" . $row_for_2015_2016['Details_Of_Work'] . "</td><td>" . $row_for_2015_2016['Amount'] . "</td></tr>";
-                }
-                echo "</tbody>";
-                echo "<tr><td></td><td><strong>Total Amount</strong></td><td><strong>" . $row_for_total_amt['Amount'] . "</strong></td></tr>";
+            }
+            echo "<tr><td></td><td><strong>Total Amount</strong></td><td><strong>" . $row_for_total_amt['Amount'] . "</strong></td></tr>";
         echo "</table>";
 echo "</div>";
 //============================================================================================
 echo "<div class='col-md-6 col-sm-6'>";
     $prabhag_num_B = $q."B";
     echo "<div class='text-center'><h3>Prabhag No ".$prabhag_num_B." </h3></div>";
-    $sql_for_2012_2013="SELECT Year , Details_Of_Work , Amount FROM work_details WHERE Prabhag_No = '".$prabhag_num_B."' && Year = '2012 - 2013'";
-    $sql_for_2013_2014="SELECT Year , Details_Of_Work , Amount FROM work_details WHERE Prabhag_No = '".$prabhag_num_B."' && Year = '2013 - 2014'";
-    $sql_for_2014_2015="SELECT Year , Details_Of_Work , Amount FROM work_details WHERE Prabhag_No = '".$prabhag_num_B."' && Year = '2014 - 2015'";
-    $sql_for_2015_2016="SELECT Year , Details_Of_Work , Amount FROM work_details WHERE Prabhag_No = '".$prabhag_num_B."' && Year = '2015 - 2016'";
 
-    $result_for_2012_2013 = mysqli_query($con,$sql_for_2012_2013);
-    $result_for_2013_2014 = mysqli_query($con,$sql_for_2013_2014);
-    $result_for_2014_2015 = mysqli_query($con,$sql_for_2014_2015);
-    $result_for_2015_2016 = mysqli_query($con,$sql_for_2015_2016);
-
+    $sql = "SELECT DISTINCT Year FROM `work_details`";
+    $result = mysqli_query($con,$sql);
+    $num_results = $result->num_rows;
+    $YEAR =array();
+    for ($i=0; $i<$num_results; $i++) { 
+        $row = mysqli_fetch_array($result);
+        $YEAR[$i] = $row['Year'];
+    }
     $sql_for_total_amt="SELECT  SUM(Amount) AS Amount FROM work_details WHERE Prabhag_No = '".$prabhag_num_B."'";
     $result_for_total_amt = mysqli_query($con,$sql_for_total_amt);
     $row_for_total_amt = mysqli_fetch_array($result_for_total_amt);
 
         echo "<table class='table-bordered table-condensed'>";
-            echo "<colgroup><col style='width:17%;'><col style='width:63%;'><col style='width:20%;'></colgroup>";
+            echo "<colgroup><col style='width:19%;'><col style='width:61%;'><col style='width:20%;'></colgroup>";
             echo "<thead><tr><th><strong>Year</strong></th><th><strong>Details Of Work</strong></th><th><strong>Amount (Rs.)</strong></th></tr></thead>";
-                echo "<tbody class='table-odd'>";
-                while($row_for_2012_2013 = mysqli_fetch_array($result_for_2012_2013))
-                {
-                    echo "<tr><td>" . $row_for_2012_2013['Year'] . "</td><td>" . $row_for_2012_2013['Details_Of_Work'] . "</td><td>" . $row_for_2012_2013['Amount'] . "</td></tr>";
+            for ($i=0; $i < $num_results; $i++)
+            { 
+                $sql_for_select = "SELECT Year , Details_Of_Work , Amount FROM work_details WHERE Prabhag_No = '".$prabhag_num_B."' && Year = '".$YEAR[$i]."' ";
+                $result_for_select = mysqli_query($con,$sql_for_select);
+                $num_of_rows = $result_for_select->num_rows;
+                if ($i%2==0)
+                {   
+                    if($num_of_rows!=0)
+                    {
+                    echo "<tbody class='table-even'>";
+                    while($row_for_select = mysqli_fetch_array($result_for_select))
+                    {
+                        echo "<tr><td>" . $row_for_select['Year'] . "</td><td>" . $row_for_select['Details_Of_Work'] . "</td><td>" . $row_for_select['Amount'] . "</td></tr>";
+                    }
+                    echo "</tbody>";
+                    }  
                 }
-                echo "</tbody>";
-                echo "<tbody class='table-even'>";
-                while($row_for_2013_2014 = mysqli_fetch_array($result_for_2013_2014))
-                {
-                    echo "<tr><td>" . $row_for_2013_2014['Year'] . "</td><td>" . $row_for_2013_2014['Details_Of_Work'] . "</td><td>" . $row_for_2013_2014['Amount'] . "</td></tr>";
+                else
+                {   
+                    if($num_of_rows!=0)
+                    {
+                    echo "<tbody class='table-odd'>";
+                    while($row_for_select = mysqli_fetch_array($result_for_select))
+                    {
+                        echo "<tr><td>" . $row_for_select['Year'] . "</td><td>" . $row_for_select['Details_Of_Work'] . "</td><td>" . $row_for_select['Amount'] . "</td></tr>";
+                    }
+                    echo "</tbody>";
+                    }
                 }
-                echo "</tbody>";
-                echo "<tbody class='table-odd'>";
-                while($row_for_2014_2015 = mysqli_fetch_array($result_for_2014_2015))
-                {
-                    echo "<tr><td>" . $row_for_2014_2015['Year'] . "</td><td>" . $row_for_2014_2015['Details_Of_Work'] . "</td><td>" . $row_for_2014_2015['Amount'] . "</td></tr>";
-                }
-                echo "</tbody>";
-                echo "<tbody class='table-even'>";
-                while($row_for_2015_2016 = mysqli_fetch_array($result_for_2015_2016))
-                {
-                    echo "<tr><td>" . $row_for_2015_2016['Year'] . "</td><td>" . $row_for_2015_2016['Details_Of_Work'] . "</td><td>" . $row_for_2015_2016['Amount'] . "</td></tr>";
-                }
-                echo "</tbody>";
-                echo "<tr><td></td><td><strong>Total Amount</strong></td><td><strong>" . $row_for_total_amt['Amount'] . "</strong></td></tr>";
+            }
+            echo "<tr><td></td><td><strong>Total Amount</strong></td><td><strong>" . $row_for_total_amt['Amount'] . "</strong></td></tr>";
         echo "</table>";
 echo "</div>";
 ?>
