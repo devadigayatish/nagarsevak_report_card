d4d79f42277fb9bcdb297554b4578ef52e4a795f
Yatish Devadiga <devadigayatish@gmail.com>
2016-07-10 13:00:46 +0530

    Downloaded the latest files from Google Drive. Updated database with the new data. Updates codes sheet.

--- sql_structure/db_structure.sql
+++ sql_structure/db_structure.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
+-- version 4.5.2
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 29, 2016 at 10:37 AM
+-- Generation Time: Jul 10, 2016 at 07:23 AM
 -- Server version: 5.7.9
--- PHP Version: 5.6.15
+-- PHP Version: 5.6.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `csv_db`
+-- Database: `nrc`
 --
 
 -- --------------------------------------------------------
