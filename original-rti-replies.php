2a73f3870617372532a9471cbb1e52bef8effc08
Yatish Devadiga <devadigayatish@gmail.com>
2016-07-18 19:53:54 +0530

    Added links to all the scanned files obtained under RTI. The files being too large, not uploading to git. They have been directly uploaded on server

--- original-rti-replies.php
+++ original-rti-replies.php
@@ -56,24 +56,34 @@ require_once('includes/functions.php');
                         <div class="col-md-12 animate-box fadeInUp animated">
                             <h3><strong> ORIGINAL RTI REPLIES </strong></h3>
                             <p>
-                                Application under Right To Information Act (RTI) were filed in 15 ward offices of Pune Municipal Corporation. The ward offices provided us with hard copies of the information. We have scanned those copies and are providing the same as it is for reference. Please download the file by clicking on the appropriate Ward office name.
+                                Applications under Right To Information Act (RTI) were filed in 15 ward offices of Pune Municipal Corporation. The ward offices provided us with hard copies of the information. We have scanned those copies and are providing the same as it is for reference. Please download the file by clicking on the appropriate Ward office name.
                             </p>
                             <br>
-                            <a href="<?php echo SITE_URL ?>uploads/original-rti-replies/aundh.pdf" target="_blank"><h3><strong> Aundh </strong></h3></a>
-                            <a href="<?php echo SITE_URL ?>uploads/original-rti-replies/bhavani-peth.pdf" target="_blank"><h3><strong> Bhawani Peth </strong></h3></a>
-                            <a href="<?php echo SITE_URL ?>uploads/original-rti-replies/bibvewadi.pdf" target="_blank"><h3><strong> Bibvewadi </strong></h3></a>
-                            <a href="<?php echo SITE_URL ?>uploads/original-rti-replies/dhankawadi.pdf" target="_blank"><h3><strong> Dhankawadi </strong></h3></a>
-                            <a href="<?php echo SITE_URL ?>uploads/original-rti-replies/dhole-patil-road.pdf" target="_blank"><h3><strong> Dhole Patil Road </strong></h3></a>
-                            <a href="<?php echo SITE_URL ?>uploads/original-rti-replies/ghole-road.pdf" target="_blank"><h3><strong> Ghole Road </strong></h3></a>
-                            <a href="<?php echo SITE_URL ?>uploads/original-rti-replies/hadapsar.pdf" target="_blank"><h3><strong> Hadapsar </strong></h3></a>
-                            <a href="<?php echo SITE_URL ?>uploads/original-rti-replies/kasba-vishrambaugwada.pdf" target="_blank"><h3><strong> Kasba Vishrambaugwada </strong></h3></a>
-                            <a href="<?php echo SITE_URL ?>uploads/original-rti-replies/kondhwa-wanavdi.pdf" target="_blank"><h3><strong> Kondhwa Wanavdi </strong></h3></a>
-                            <a href="<?php echo SITE_URL ?>uploads/original-rti-replies/kothrud.pdf" target="_blank"><h3><strong> Kothrud </strong></h3></a>
-                            <a href="<?php echo SITE_URL ?>uploads/original-rti-replies/nagar-road.pdf" target="_blank"><h3><strong> Nagar Road </strong></h3></a>
-                            <a href="<?php echo SITE_URL ?>uploads/original-rti-replies/sahakarnagar.pdf" target="_blank"><h3><strong> Sahakarnagar </strong></h3></a>
-                            <a href="<?php echo SITE_URL ?>uploads/original-rti-replies/tilak-road.pdf" target="_blank"><h3><strong> Tilak Road </strong></h3></a>
-                            <a href="<?php echo SITE_URL ?>uploads/original-rti-replies/warje-karvenagar.pdf" target="_blank"><h3><strong> Warje Karvenagar </strong></h3></a>
-                            <a href="<?php echo SITE_URL ?>uploads/original-rti-replies/yerawda-sangamwadi.pdf" target="_blank"><h3><strong> Yerawda Sangamwadi </strong></h3></a>
+                            <a href="<?php echo SITE_URL ?>uploads/original-rti-replies/Aundh_2012-2016.pdf" target="_blank"><h3><strong> Aundh </strong></h3></a>
+                            <a href="<?php echo SITE_URL ?>uploads/original-rti-replies/Bhavani_Peth_2012-2016.pdf" target="_blank"><h3><strong> Bhawani Peth </strong></h3></a>
+                            <a href="<?php echo SITE_URL ?>uploads/original-rti-replies/Bibwewadi_2012-2016.pdf" target="_blank"><h3><strong> Bibvewadi </strong></h3></a>
+                            <a href="<?php echo SITE_URL ?>uploads/original-rti-replies/Dhankawadi_2012-2016.pdf" target="_blank"><h3><strong> Dhankawadi </strong></h3></a>
+                            <a href="<?php echo SITE_URL ?>uploads/original-rti-replies/Dhole_Patil_Road_2012-2016.pdf" target="_blank"><h3><strong> Dhole Patil Road </strong></h3></a>
+                            <a href="<?php echo SITE_URL ?>uploads/original-rti-replies/Ghole_Road_2012-2016.pdf" target="_blank"><h3><strong> Ghole Road </strong></h3></a>
+                            <a href="<?php echo SITE_URL ?>uploads/original-rti-replies/Hadapsar_2012-2016.pdf" target="_blank"><h3><strong> Hadapsar </strong></h3></a>
+                            <a href="<?php echo SITE_URL ?>uploads/original-rti-replies/Kasba_Vishrambaugwada_2012-2016.pdf" target="_blank"><h3><strong> Kasba Vishrambaugwada </strong></h3></a>
+                            <a href="<?php echo SITE_URL ?>uploads/original-rti-replies/Kondhwa_2012-2016.pdf" target="_blank"><h3><strong> Kondhwa Wanavdi </strong></h3></a>
+                            <a href="<?php echo SITE_URL ?>uploads/original-rti-replies/Kothrud_2012-2016.pdf" target="_blank"><h3><strong> Kothrud </strong></h3></a>
+                            <a href="<?php echo SITE_URL ?>uploads/original-rti-replies/Nagar_Road_2012-2016.pdf" target="_blank"><h3><strong> Nagar Road </strong></h3></a>
+                            <a href="<?php echo SITE_URL ?>uploads/original-rti-replies/Sahkarnagar_2012-2016.pdf" target="_blank"><h3><strong> Sahakarnagar </strong></h3></a>
+                            <a href="<?php echo SITE_URL ?>uploads/original-rti-replies/Tilak_Road_2012-2016.pdf" target="_blank"><h3><strong> Tilak Road </strong></h3></a>
+                            <a href="<?php echo SITE_URL ?>uploads/original-rti-replies/Warje_Karvenagar_2012-2016.pdf" target="_blank"><h3><strong> Warje Karvenagar </strong></h3></a>
+                            <a href="<?php echo SITE_URL ?>uploads/original-rti-replies/Yerawda_2012-16.pdf" target="_blank"><h3><strong> Yerawda Sangamwadi </strong></h3></a>
+                            <hr>
+                            <p>
+                                Applications under Right To Information Act (RTI) were filed in main building of Pune Municipal Corporation & Commissioner of Police, Pune. The respective offices provided us with hard copies of the information. We have scanned those copies and are providing the same as it is for reference. Please download the required files.
+                            </p>
+                            <br>
+                            <a href="<?php echo SITE_URL ?>uploads/original-rti-replies/Nagarsevak_List_Nov_2015.pdf" target="_blank"><h3><strong> Nagarsevak List (Updated till November 2015) </strong></h3></a>
+                            <a href="<?php echo SITE_URL ?>uploads/original-rti-replies/Attendance_2012-2016.pdf" target="_blank"><h3><strong> Attendance List </strong></h3></a>
+                            <a href="<?php echo SITE_URL ?>uploads/original-rti-replies/Questions_raised_2012-2016.pdf" target="_blank"><h3><strong> Questions Raised </strong></h3></a>
+                            <a href="<?php echo SITE_URL ?>uploads/original-rti-replies/criminal_record_2012-2016.pdf" target="_blank"><h3><strong> Criminal Record List </strong></h3></a>
+                            <a href="<?php echo SITE_URL ?>uploads/original-rti-replies/Govt_Guidelines_for_utilization_of_ward_level_funds.pdf" target="_blank"><h3><strong> Government Guidelines For Utilization Of Ward Level Funds </strong></h3></a>
                         </div>
                     </div>
                 </div>
